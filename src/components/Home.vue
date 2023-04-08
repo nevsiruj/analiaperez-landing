4b394df391fefd67c39cf8f5a015fcf11899ab15
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-04-07 21:54:38 -0300

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,219 @@
+<template>
+  <header class="bg-gradient-to-r from-fuchsia-400 to-cyan-500 py-4">
+    <div class="container mx-auto flex justify-between items-center">
+      <a href="#" class="text-white font-bold text-xl ml-2">Dance MIX</a>
+      <nav>
+        <a href="#" class="text-white hover:underline mx-4">Inicio</a>
+        <a href="#" class="text-white hover:underline mx-4">Beneficios</a>
+        <a href="#" class="text-white hover:underline mx-4">Contacto</a>
+      </nav>
+    </div>
+  </header>
+
+  <section class="bg-gray-100 py-12">
+    <div class="container mx-auto px-4">
+      <div class="flex flex-wrap -mx-4">
+        <div class="w-full md:w-1/2 px-4 mb-8">
+          <h2 class="text-2xl font-bold mb-4 text-gray-800">
+            Aprende a bailar conmigo
+          </h2>
+          <p class="text-lg mb-4">
+            ¿Quieres aprender a bailar de forma divertida y fácil? Soy un
+            instructor de baile experimentado y estoy aquí para ayudarte.
+          </p>
+          <p class="text-lg mb-4">
+            Ofrezco clases de baile para todos los niveles y estilos, desde
+            salsa y bachata hasta tango y hip hop. Mis clases son divertidas y
+            accesibles, y me aseguro de que todos mis estudiantes aprendan a su
+            propio ritmo.
+          </p>
+          <a
+            href="#"
+            class="
+              bg-blue-500
+              hover:bg-blue-600
+              text-white
+              py-2
+              px-4
+              rounded
+            "
+            >¡Inscríbete ahora!</a
+          >
+
+
+        <div class="card m-3">
+
+
+          <img
+            src="https://files.fm/thumb_show.php?i=4hcan2s3p"
+            alt="Dance Mix"
+            class="rounded-lg mb-4"
+          />
+        </div>
+        </div>
+
+
+        <div class="w-full md:w-1/2 px-4 mb-8">
+          <h2 class="text-2xl font-bold mb-4 text-gray-800">Sobre el baile</h2>
+          <p class="text-lg mb-4">
+            El baile es una forma de expresión artística y cultural que ha
+            existido desde tiempos prehistóricos. En todas las culturas del
+            mundo, el baile se ha utilizado para celebrar, comunicar y expresar
+            emociones y sentimientos.
+          </p>
+          <p class="text-lg mb-4">
+            Además de ser una forma de arte, el baile también tiene muchos
+            beneficios para la salud, como mejorar la coordinación, la
+            flexibilidad y la resistencia física. También puede ser una forma
+            divertida de socializar y conocer gente nueva.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Sección de beneficios -->
+  <section class="bg-white py-12">
+    <div class="container mx-auto px-4">
+      <h2 class="text-2xl font-bold mb-8 text-gray-800">
+        Beneficios de bailar
+      </h2>
+      <div class="flex flex-wrap -mx-4">
+        <div class="w-full md:w-1/3 px-4 mb-8">
+          <div class="bg-blue-500 text-white py-6 px-4 rounded-lg">
+            <h3 class="font-bold text-lg mb-2">Mejora la salud</h3>
+            <p class="text-sm">
+              Bailar es una excelente forma de hacer ejercicio y mejorar la
+              salud cardiovascular, la resistencia y la fuerza muscular.
+            </p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/3 px-4 mb-8">
+          <div class="bg-green-500 text-white py-6 px-4 rounded-lg">
+            <h3 class="font-bold text-lg mb-2">Reduce el estrés</h3>
+            <p class="text-sm">
+              Bailar puede ayudar a reducir el estrés y la ansiedad, ya que
+              libera endorfinas y promueve la relajación.
+            </p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/3 px-4 mb-8">
+          <div class="bg-yellow-500 text-white py-6 px-4 rounded-lg">
+            <h3 class="font-bold text-lg mb-2">Mejora la autoestima</h3>
+            <p class="text-sm">
+              Bailar puede ayudar a mejorar la autoestima y la confianza, ya que
+              permite expresarse y ser creativo.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Sección de llamado a la acción -->
+  <section class="bg-gray-900 py-12">
+    <div class="container mx-auto px-4">
+      <h2 class="text-2xl font-bold mb-4 text-white">¡Comienza a bailar hoy mismo!</h2>
+      <p class="text-lg text-white mb-8">No esperes más para descubrir todos los beneficios del baile. ¡Inscríbete en nuestras clases ahora y empieza a disfrutar de la música y el movimiento!</p>
+      <a href="#" class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">¡Inscríbete ahora!</a>
+    </div>
+  </section>
+
+
+
+
+  <section class="bg-cool-gray-100 py-16">
+    <div class="container mx-auto">
+      <h2 class="text-2xl font-bold mb-">Sobre Analia Perz</h2>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div class="card m-2">
+          <img
+            src="https://files.fm/thumb_show.php?i=7vvyr2y3g"
+            alt="Foto de Gustavo Suarez"
+            class="rounded-lg card"
+          />
+        </div>
+     <div class="m-2">
+  <div class="card">
+    <p class="text-gray-700 text-lg mb-4">
+      Analía Pérez es una reconocida profesora de baile de Villa Gesell,
+      comprometida con la enseñanza y la difusión de la danza en su comunidad.
+      Nació y creció en la ciudad, donde ha desempeñado un papel activo en la
+      escena del baile desde una temprana edad.
+    </p>
+    <p class="text-gray-700 text-lg mb-4">
+      Es Licenciada en Danza por la Universidad Nacional de las Artes y ha
+      trabajado como instructora de baile en varios estudios y academias antes
+      de fundar su propia escuela en 2010. Desde entonces, ha enseñado a
+      numerosos estudiantes de todas las edades y niveles de habilidad.
+    </p>
+    <p class="text-gray-700 text-lg mb-4">
+      En el ámbito del baile, Analía ha sido reconocida por su talento y su
+      dedicación a la enseñanza. Ha participado en numerosos festivales y
+      competencias de danza, y ha ganado varios premios por sus coreografías y
+      su estilo innovador. Ahora, como propietaria y directora de su propia
+      escuela de baile, quiere compartir su pasión y su experiencia con todos
+      aquellos que quieran aprender y disfrutar de la danza.
+    </p>
+  </div>
+</div>
+
+          <a
+            href="#"
+            class="
+              bg-blue-500
+              hover:bg-blue-600
+              text-white
+              font-bold
+              py-2
+              px-4
+              rounded
+            "
+            >Contacta con Analia</a
+          >
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer class="bg-gradient-to-r from-blue-500 to-purple-500 text-white py-6">
+    <div class="container mx-auto px-4">
+      <p class="text-center">Dance MIX. &copy; 2023. </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 5px; /* ajusta este valor para modificar la curvatura de las esquinas de la tarjeta */
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
