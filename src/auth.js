f22f32ce5f0e5dcd7ca8526522c4e0a7c30d4873
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-04-20 14:15:54 -0300

    1 file modified

--- src/auth.js
+++ src/auth.js
@@ -1,8 +1,8 @@
 // const USERNAME = 'user399';
 // const PASSWORD = 'cK7@nDh8';
 
-const USERNAME = 'user2023';
-const PASSWORD = 'analiadance.2023';
+const USERNAME = 'studio2023';
+const PASSWORD = 'mixdance';
 
 export function authenticate(username, password) {
   return new Promise((resolve, reject) => {
