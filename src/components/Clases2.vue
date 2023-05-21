9ab1600b9839dfcd60c233a4fb26563514c25061
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-05-21 12:14:50 -0300

    1 file modified

--- src/components/Clases2.vue
+++ src/components/Clases2.vue
@@ -1,4 +1,73 @@
 <template>
+  <div
+    id="login-modal"
+    class="fixed z-10 inset-0 overflow-y-auto flex items-center justify-center"
+    v-show="showModal"
+  >
+    <div
+      class="
+        bg-white
+        rounded-lg
+        overflow-hidden
+        shadow-xl
+        transform
+        transition-all
+        sm:max-w-lg sm:w-full
+      "
+    >
+      <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div class="mt-3 text-center sm:mt-0 sm:text-left w-full">
+          <h3 class="text-2xl leading-6 font-medium text-gray-900">
+            Ingreso al sistema
+          </h3>
+          <div class="mt-2">
+            <input
+              type="text"
+              v-model="username"
+              placeholder="Usuario"
+              class="my-2 p-2 w-full border-2 border-gray-300 rounded-md"
+            />
+            <input
+              type="password"
+              v-model="password"
+              placeholder="Contraseña"
+              class="my-2 p-2 w-full border-2 border-gray-300 rounded-md"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+        <button
+          @click="checkCredentials"
+          class="
+            inline-flex
+            justify-center
+            w-full
+            rounded-md
+            border border-transparent
+            px-4
+            py-2
+            bg-green-600
+            text-base
+            leading-6
+            font-medium
+            text-white
+            shadow-sm
+            hover:bg-green-500
+            focus:outline-none focus:border-green-700 focus:shadow-outline-green
+            transition
+            ease-in-out
+            duration-150
+            sm:text-sm sm:leading-5
+          "
+        >
+          Ingresar
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <!--Contenido-->
   <section class="bg-gray-100 py-10">
     <div class="container mx-auto text-center">
       <h2 class="text-3xl font-bold mb-4 text-purple-500">
@@ -16,6 +85,7 @@
             frameborder="0"
             allowfullscreen
           ></iframe>
+
           <div class="p-4">
             <h3 class="text-2xl font-bold mb-2">00 - Presentanción</h3>
           </div>
@@ -229,6 +299,29 @@
 export default {
   name: 'Clases',
   props: {},
+  data() {
+    return {
+      showModal: true,
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    checkCredentials() {
+      const hardcodedUsername = 'admin';
+      const hardcodedPassword = 'password';
+
+      if (
+        this.username === hardcodedUsername &&
+        this.password === hardcodedPassword
+      ) {
+        alert('Inicio de sesión exitoso');
+        this.showModal = false;
+      } else {
+        alert('Credenciales incorrectas');
+      }
+    },
+  },
 };
 </script>
 
