10c7f82a6fcc03b69cc612ec6aeb277b65f58e72
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-06-10 23:08:08 -0300

    1 file modified

--- src/components/PreClases.vue
+++ src/components/PreClases.vue
@@ -3,9 +3,39 @@
     <div class="max-w-4xl mx-auto px-6">
       <div class="flex flex-col md:flex-row items-center justify-between">
         <div class="md:w-1/2 mb-8 md:mb-0 text-center md:text-left">
-          <h2 class="text-3xl font-bold mb-4">
-            ¡Bienvenido al curso de baile!
-          </h2>
+          <h2 class="text-3xl font-bold mb-4">Ingresa al Curso 1</h2>
+          <p class="text-lg mb-6">
+            Aprende los mejores movimientos y entrenamientos con Analia Perez.
+            Fit Dance. Capacitate con nosotros, ingresa ahora!
+          </p>
+          <a
+            href="/clases2"
+            class="
+              bg-blue-500
+              hover:bg-blue-700
+              text-white
+              font-bold
+              py-2
+              px-4
+              rounded
+              inline-flex
+              items-center
+              mb-4
+            "
+          >
+            <i class="fas fa-sign-in-alt mr-2"></i>
+            <span>Acceder al Curso</span>
+          </a>
+          <br />
+          <a
+            href="https://wa.me/+5492255427173"
+            class="text-gray-600 hover:text-gray-800 text-sm"
+          >
+            ¿Olvidaste tu usuario o contraseña?
+          </a>
+        </div>
+        <div class="md:w-1/2 mb-8 md:mb-0 text-center md:text-left">
+          <h2 class="text-3xl font-bold mb-4">Ingresa al Curso 2</h2>
           <p class="text-lg mb-6">
             Aprende los mejores movimientos de baile con Analia Perez. Accede
             ahora y comienza a bailar como un verdadero experto. 💃
